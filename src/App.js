ee28aab4c371e912e8191594dbe996b7cc732d98
Wayne <wayne.cen@gmail.com>
2022-06-20 20:51:47 -0400

    Update TravelEntry component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
+import TravelEntry from "./components/TravelEntry";
+import data from "./data";
 
 function App() {
+	const entries = data.map((entry) => {
+		return (
+			<TravelEntry key={entry.id} img={entry.imageUrl} entry={entry} />
+		);
+	});
+
 	return (
 		<div className="App">
 			<Header />
+			{entries}
 		</div>
 	);
 }
--- src/components/TravelEntry.js
+++ src/components/TravelEntry.js
@@ -1,1 +1,34 @@
 import React from "react";
+import Divider from "./Divider";
+// import mountFuji from "../img/mountFuji.png";
+
+export default function TravelEntry(props) {
+	return (
+		<div className="travelEntry flex-col">
+			<div className="flex-row">
+				<img
+					src={`../img/${props.entry.imageUrl}`}
+					alt=""
+					className="entry--img"
+				/>
+				<div className="flex-col">
+					<div className="wrapper__location flex-row">
+						<h5 className="location">{props.entry.location}</h5>
+						<a
+							href={`${props.entry.googleMapsUrl}`}
+							className="link__googleMaps"
+						>
+							View on Google Maps
+						</a>
+					</div>
+					<h3 className="title">{props.entry.title}</h3>
+					<h4 className="startEndDate">
+						{props.entry.startDate} - {props.entry.endDate}
+					</h4>
+					<p className="description">{props.entry.description}</p>
+				</div>
+			</div>
+			<Divider />
+		</div>
+	);
+}
